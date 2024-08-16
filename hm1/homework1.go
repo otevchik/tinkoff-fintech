71cec6bb683668daf63d1ca488a529ced7259e8b
Олег Вишняков <oleg.vishnyakov.02@inbox.ru>
2024-08-16 18:13:56 +0300

    done

---
+++ hm1/homework1.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func sandglass(size int, char rune, color string) {
+	for i := 0; i < size; i++ {
+		start := i
+		end := size - 1 - i
+		for j := 0; j < size; j++ {
+			switch {
+			case (i == 0 || i == (size - 1)):
+				fmt.Printf(color + "%c", char)
+			case ((j == start || j == end) && start != end):
+				fmt.Printf(color + "%c", char)
+			case (start == end && j == start):
+				fmt.Printf(color + "%c", char)
+			default:
+				fmt.Print(" ")
+			}
+		}
+		if (i != size - 1) {
+			fmt.Println()
+		}
+	}
+}
+
+func main () {
+	size := 14
+	color := "\033[31m"
+	char := 'B'
+	sandglass(size, char, color)
+}
